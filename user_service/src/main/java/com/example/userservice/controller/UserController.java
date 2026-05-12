926a7b97a41c8ab07cb9cfe9de96c10455a89517
Reitzzz <1608886337@qq.com>
2026-05-12 10:52:23 +0800

    将UserService的配置文件放至Nacos 并在UserController中编写测试

--- user_service/src/main/java/com/example/userservice/controller/UserController.java
+++ user_service/src/main/java/com/example/userservice/controller/UserController.java
@@ -3,10 +3,14 @@ package com.example.userservice.controller;
 import com.example.common.entity.User;
 import com.example.userservice.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/user")
@@ -14,11 +18,21 @@ public class UserController {
 
     @Autowired
     private UserMapper userMapper;
+    @Value("${pattern.dateformat}")
+    private String dateformat;
 
     @GetMapping("/{uid}")
     public User findUserById(@PathVariable("uid") Integer uid) {
         System.out.println("call user service");
         return userMapper.findUserByUid(uid);
     }
+
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
+
+
+
 }
 
