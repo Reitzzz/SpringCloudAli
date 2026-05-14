a76a02ac9c0718eaccf9b4e70471e14e284552ef
Reitzzz <1608886337@qq.com>
2026-05-14 16:43:05 +0800

    在BorrowController中加入热点参数限流

--- borrow_service/src/main/java/com/example/borrowservice/controller/BorrowController.java
+++ borrow_service/src/main/java/com/example/borrowservice/controller/BorrowController.java
@@ -1,13 +1,11 @@
 package com.example.borrowservice.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.fastjson.JSONObject;
 import com.example.borrowservice.service.BorrowService;
 import com.example.common.dto.UserBorrowDetail;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/borrow")
@@ -27,6 +25,14 @@ public class BorrowController {
         object.put("success", false);
         object.put("massage", "您的请求频率过快，请稍后再试！");
         return object;
+    }
+
+    @RequestMapping("/test")
+    @SentinelResource("test")   //注意这里需要添加@SentinelResource才可以，用户资源名称就使用这里定义的资源名称
+    String findUserBorrows2(@RequestParam(value = "a", required = false) Integer a,
+                            @RequestParam(value = "b", required = false) Integer b,
+                            @RequestParam(value = "c",required = false) Integer c) {
+        return "请求成功！a = "+a+", b = "+b+", c = "+c;
     }
 }
 
